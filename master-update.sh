2e21889c1e5e98c5c7ce5ca2c96b9ec3ac746aa7
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2020-11-17 08:39:46 +0000

    edit 1
    to run:
    
    sudo sh master-update.sh
    
    NOTICE: there will be errors. ignore them.

---
+++ master-update.sh
@@ -0,0 +1,10 @@
+sudo
+echo "the update that works on all"
+sudo apt update -y
+sudo apt upgrade -y
+snap refresh -y
+sudo dnf check-update -y
+sudo dnf upgrade -y
+sudo yum update  -y
+pacman -Syu -y
+zypper up -y
